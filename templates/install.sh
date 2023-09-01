df4471c26c64ca00ab4d6f8a1b435a5c22677475
Joshua M. Boniface <joshua@boniface.me>
2023-09-01 15:42:00 -0400

    Fix bug with active TTYs

--- templates/install.sh
+++ templates/install.sh
@@ -17,7 +17,7 @@ this_tty=$( tty | sed -e "s:/dev/::" )
 echo "This TTY: ${this_tty}"
 echo
 
-if [[ ${#active_ttys} -gt 1 ]]; then
+if [[ ${#active_ttys[@]} -gt 1 ]]; then
     if [[ "${active_ttys[@]}" =~ "ttyS" ]]; then
         if grep -q -E -o "tty[0-9]+" <<<"${this_tty}"; then
             echo "Found more than one TTY and at least one serial TTY!"
