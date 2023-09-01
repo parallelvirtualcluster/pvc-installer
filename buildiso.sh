a2b257ef9edce46d2edbdba76272b55c21d560a8
Joshua M. Boniface <joshua@boniface.me>
2023-09-01 15:41:53 -0400

    Use amd64 EFI GRUB explicitly

--- buildiso.sh
+++ buildiso.sh
@@ -95,13 +95,13 @@ prepare_iso() {
 
 prepare_rootfs() {
     echo -n "Preparing Debian live installation via debootstrap... "
-    SQUASHFS_PKGLIST="mdadm,lvm2,parted,gdisk,debootstrap,grub-pc,grub-efi,linux-image-amd64,sipcalc,live-boot,dosfstools"
+    SQUASHFS_PKGLIST="mdadm,lvm2,parted,gdisk,debootstrap,grub-pc,grub-efi-amd64,linux-image-amd64,sipcalc,live-boot,dosfstools"
     if [[ ! -d artifacts/debootstrap ]]; then
         sudo /usr/sbin/debootstrap \
             --include=${SQUASHFS_PKGLIST} \
             buster \
             artifacts/debootstrap/ \
-            http://localhost:3142/ftp.ca.debian.org/debian &>/dev/null || fail "Error performing debootstrap."
+            http://localhost:3142/ftp.ca.debian.org/debian &>debootstrap.log || fail "Error performing debootstrap."
             sudo chroot artifacts/debootstrap/ apt clean &>/dev/null || fail "Error cleaning apt cache in debootstrap."
     fi
     sudo rsync -au artifacts/debootstrap/ ${tempdir}/rootfs/ &>/dev/null || fail "Error copying debootstrap to tempdir."
